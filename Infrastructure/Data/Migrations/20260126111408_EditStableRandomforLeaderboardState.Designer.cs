fb668d075a11530b455b03f0df1c1e101581d5bc
Quang Hau <l3acasha@gmail.com>
2026-01-27 18:25:58 +0700

    feat(db): Migrate LeaderboardState.StableRandom to integer type

--- Infrastructure/Data/Migrations/20260126111408_EditStableRandomforLeaderboardState.Designer.cs
+++ Infrastructure/Data/Migrations/20260126111408_EditStableRandomforLeaderboardState.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FidoDino.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace fidodino.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(FidoDinoDbContext))]
-    partial class FidoDinoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260126111408_EditStableRandomforLeaderboardState")]
+    partial class EditStableRandomforLeaderboardState
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Infrastructure/Data/Migrations/20260126111408_EditStableRandomforLeaderboardState.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace fidodino.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditStableRandomforLeaderboardState : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+        -- 1. Fix dữ liệu rỗng / null trước
+        UPDATE "LeaderboardStates"
+        SET "StableRandom" = '0'
+        WHERE "StableRandom" IS NULL
+           OR "StableRandom" = '';
+
+        -- 2. Ép kiểu sang integer
+        ALTER TABLE "LeaderboardStates"
+        ALTER COLUMN "StableRandom" TYPE integer
+        USING "StableRandom"::integer;
+    """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "StableRandom",
+                table: "LeaderboardStates",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+    }
+}
